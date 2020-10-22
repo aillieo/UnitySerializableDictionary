26685f17e616b14697f5cc5149a2fc7d27561141
aillieo <aillieo@163.com>
2020-10-22 23:29:58 +0800

    first commit

---
+++ SerializableDictionary/Scripts/Runtime/SerializableDictionary.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace AillieoUtils
+{
+    [Serializable]
+    public class SerializableDictionary<TKey, TValue> : IDictionary<TKey, TValue>, ISerializationCallbackReceiver
+    {
+        private readonly Dictionary<TKey, TValue> dictionary = new Dictionary<TKey, TValue>();
+        [SerializeField]
+        private readonly List<TKey> keys = new List<TKey>();
+        [SerializeField]
+        private readonly List<TValue> values = new List<TValue>();
+
+        public TValue this[TKey key]
+        {
+            get { return dictionary[key]; }
+            set { dictionary[key] = value; }
+        }
+
+        public ICollection<TKey> Keys
+        {
+            get { return dictionary.Keys; }
+        }
+
+        public ICollection<TValue> Values
+        {
+            get { return dictionary.Values; }
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            dictionary.Add(key, value);
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            return dictionary.ContainsKey(key);
+        }
+
+        public bool Remove(TKey key)
+        {
+            return dictionary.Remove(key);
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            return dictionary.TryGetValue(key, out value);
+        }
+
+        public void Clear()
+        {
+            dictionary.Clear();
+        }
+
+        public int Count
+        {
+            get { return dictionary.Count; }
+        }
+
+        bool ICollection<KeyValuePair<TKey, TValue>>.IsReadOnly
+        {
+            get { return (dictionary as ICollection<KeyValuePair<TKey, TValue>>).IsReadOnly; }
+        }
+
+        void ICollection<KeyValuePair<TKey, TValue>>.Add(KeyValuePair<TKey, TValue> item)
+        {
+            (dictionary as ICollection<KeyValuePair<TKey, TValue>>).Add(item);
+        }
+
+        bool ICollection<KeyValuePair<TKey, TValue>>.Contains(KeyValuePair<TKey, TValue> item)
+        {
+            return (dictionary as ICollection<KeyValuePair<TKey, TValue>>).Contains(item);
+        }
+
+        void ICollection<KeyValuePair<TKey, TValue>>.CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+        {
+            (dictionary as ICollection<KeyValuePair<TKey, TValue>>).CopyTo(array, arrayIndex);
+        }
+
+        bool ICollection<KeyValuePair<TKey, TValue>>.Remove(KeyValuePair<TKey, TValue> item)
+        {
+            return (dictionary as ICollection<KeyValuePair<TKey, TValue>>).Remove(item);
+        }
+
+        IEnumerator<KeyValuePair<TKey, TValue>> IEnumerable<KeyValuePair<TKey, TValue>>.GetEnumerator()
+        {
+            return (dictionary as IEnumerable<KeyValuePair<TKey, TValue>>).GetEnumerator();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return dictionary.GetEnumerator();
+        }
+
+        public void OnBeforeSerialize()
+        {
+            keys.Clear();
+            values.Clear();
+            foreach(var pair in dictionary)
+            {
+                keys.Add(pair.Key);
+                values.Add(pair.Value);
+            }
+        }
+
+        public void OnAfterDeserialize()
+        {
+            dictionary.Clear();
+
+            if (keys.Count != values.Count)
+            {
+                throw new Exception("invalid serialized data");
+            }
+
+            for (var i = 0; i < keys.Count; ++i)
+            {
+                dictionary[keys[i]] = values[i];
+            }
+        }
+    }
+}
---
+++ Tests/TestScript.cs
@@ -0,0 +1,9 @@
+using AillieoUtils;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    public SerializableDictionary<string, string> sd = new SerializableDictionary<string, string>();
+}
