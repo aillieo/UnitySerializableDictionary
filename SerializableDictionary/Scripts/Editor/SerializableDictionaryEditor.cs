25efd3bb8e960fd5cf5e7dc0a3a4dc4b4f248426
aillieo <aillieo@163.com>
2022-06-12 15:59:07 +0800

    use reorderablelist in inspector

--- SerializableDictionary/Scripts/Editor/SerializableDictionaryEditor.cs
+++ SerializableDictionary/Scripts/Editor/SerializableDictionaryEditor.cs
@@ -4,19 +4,21 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEditor;
+using UnityEditorInternal;
 
 namespace AillieoUtils
 {
     [CustomPropertyDrawer(typeof(SerializableDictionary<,>), true)]
     internal class SerializableDictionaryEditor : PropertyDrawer
     {
-        private readonly Dictionary<string, bool> expandState = new Dictionary<string, bool>();
+        private SerializedProperty serializedProperty;
+        private GUIContent label;
+        private ReorderableList reorderableList;
 
-        private static readonly float spaceHeight = 4;
-        private static readonly float spaceWidth = 4;
-        private static readonly float buttonWidth = EditorGUIUtility.singleLineHeight;
-        private static readonly float folderWidth = EditorGUIUtility.singleLineHeight * 1.0f;
+        private static readonly float spaceHeight = EditorGUIUtility.singleLineHeight * 0.5f;
+        private static readonly float dropAreaHeight = EditorGUIUtility.singleLineHeight * 2f;
         private static readonly Color lightGray = new Color(0.9f, 0.9f, 0.9f, 1f);
+        private static readonly float emptyHeight = 24f;
 
         private Type cachedValueType;
         private Type valueType
@@ -47,6 +49,19 @@ namespace AillieoUtils
 
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
+            this.serializedProperty = property;
+            this.label = label;
+
+            if (!serializedProperty.isExpanded)
+            {
+                return EditorGUIUtility.singleLineHeight;
+            }
+
+            if (reorderableList == null)
+            {
+                CreateReorderableList();
+            }
+
             SerializedProperty keys = property.FindPropertyRelative("keys");
             SerializedProperty values = property.FindPropertyRelative("values");
 
@@ -54,44 +69,127 @@ namespace AillieoUtils
 
             float height = 0;
             height += EditorGUIUtility.singleLineHeight;
+
+            height += GetHeightForAllPairs();
+
             height += spaceHeight;
-            for (int i = 0; i < count; ++i)
-            {
-                SerializedProperty key = keys.GetArrayElementAtIndex(i);
-                SerializedProperty value = values.GetArrayElementAtIndex(i);
-
-                if (DrawAsSingleLine(key.propertyType, value.propertyType))
-                {
-                    height += EditorGUIUtility.singleLineHeight;
-                }
-                else
-                {
-                    height += EditorGUI.GetPropertyHeight(key);
-                    string keyPropertyPath = key.propertyPath;
-
-                    if (expandState.TryGetValue(keyPropertyPath, out bool expand) && expand)
-                    {
-                        height += EditorGUI.GetPropertyHeight(value);
-                    }
-                }
-
-                height += spaceHeight;
-            }
-
-            height += EditorGUIUtility.singleLineHeight;
 
             bool drawDropArea = typeof(UnityEngine.Object).IsAssignableFrom(valueType);
             if (drawDropArea)
             {
-                height += EditorGUIUtility.singleLineHeight * 2f;
+                height += dropAreaHeight;
             }
 
-            height += spaceHeight;
+            int selected = reorderableList.index;
+            if (selected >= 0 && selected < count)
+            {
+                SerializedProperty key = keys.GetArrayElementAtIndex(selected);
+                SerializedProperty value = values.GetArrayElementAtIndex(selected);
+                bool drawAsSingleLine = DrawAsSingleLine(key.propertyType, value.propertyType);
+                if (!drawAsSingleLine)
+                {
+                    height += spaceHeight;
+                    height += EditorGUI.GetPropertyHeight(value, GUIContent.none, value.isExpanded);
+                }
+            }
+
             return height;
+        }
+
+        private void CreateReorderableList()
+        {
+            this.reorderableList = new ReorderableList(this.serializedProperty.serializedObject, this.serializedProperty.FindPropertyRelative("keys"));
+            this.reorderableList.drawElementCallback = DrawElementCallback;
+            this.reorderableList.elementHeightCallback = ElementHeightCallback;
+            this.reorderableList.onAddCallback = OnAddCallback;
+            this.reorderableList.onRemoveCallback = OnRemoveCallback;
+            this.reorderableList.onReorderCallbackWithDetails = OnReorderCallbackWithDetails;
+            this.reorderableList.headerHeight = 1f;
+            this.reorderableList.drawHeaderCallback = DrawHeaderCallback;
+        }
+
+        private float ElementHeightCallback(int index)
+        {
+            SerializedProperty keys = serializedProperty.FindPropertyRelative("keys");
+            SerializedProperty values = serializedProperty.FindPropertyRelative("values");
+            SerializedProperty key = keys.GetArrayElementAtIndex(index);
+            SerializedProperty value = values.GetArrayElementAtIndex(index);
+
+            return GetHeightForPair(key, value);
+        }
+
+        private void DrawElementCallback(Rect rect, int index, bool isActive, bool isFocused)
+        {
+            SerializedProperty keys = serializedProperty.FindPropertyRelative("keys");
+            SerializedProperty values = serializedProperty.FindPropertyRelative("values");
+            SerializedProperty key = keys.GetArrayElementAtIndex(index);
+            SerializedProperty value = values.GetArrayElementAtIndex(index);
+
+            if (DrawAsSingleLine(key.propertyType, value.propertyType))
+            {
+                Rect half = rect;
+                half.width = rect.width * 0.5f;
+                EditorGUI.PropertyField(half, key, GUIContent.none);
+                half.x += half.width;
+                EditorGUI.PropertyField(half, value, GUIContent.none);
+            }
+            else
+            {
+                float heightForKey = EditorGUI.GetPropertyHeight(key);
+
+                rect.height = heightForKey;
+                EditorGUI.PropertyField(rect, key, GUIContent.none);
+
+                rect.y += heightForKey;
+                rect.height = heightForKey;
+            }
+        }
+
+        private void OnAddCallback(ReorderableList list)
+        {
+            SerializedProperty keys = serializedProperty.FindPropertyRelative("keys");
+            SerializedProperty values = serializedProperty.FindPropertyRelative("values");
+            int count = keys.arraySize;
+            keys.InsertArrayElementAtIndex(count);
+            values.InsertArrayElementAtIndex(count);
+        }
+
+        private void OnRemoveCallback(ReorderableList list)
+        {
+            SerializedProperty keys = serializedProperty.FindPropertyRelative("keys");
+            SerializedProperty values = serializedProperty.FindPropertyRelative("values");
+            int index = list.index;
+            int last = keys.arraySize - 1;
+
+            if (index != last)
+            {
+                keys.MoveArrayElement(index, last);
+                values.MoveArrayElement(index, last);
+            }
+            else
+            {
+                list.index = last - 1;
+            }
+
+            values.DeleteArrayElementAtIndex(last);
+            keys.DeleteArrayElementAtIndex(last);
+        }
+
+        private void OnReorderCallbackWithDetails(ReorderableList list, int oldIndex, int newIndex)
+        {
+            SerializedProperty values = serializedProperty.FindPropertyRelative("values");
+            values.MoveArrayElement(oldIndex, newIndex);
+        }
+
+        private void DrawHeaderCallback(Rect rect)
+        {
         }
 
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
+            this.serializedProperty = property;
+            this.label = label;
+
             GUI.Box(position, GUIContent.none);
 
             SerializedProperty keys = property.FindPropertyRelative("keys");
@@ -100,127 +198,94 @@ namespace AillieoUtils
             int count = keys.arraySize;
 
             Rect rect = position;
-
             rect.height = EditorGUIUtility.singleLineHeight;
-            GUI.Label(rect, $"{label.text} ({keys.arraySize})");
 
-            rect.width -= spaceWidth * 2f;
-            rect.x += spaceWidth;
+            serializedProperty.isExpanded = GUI.Toggle(rect, serializedProperty.isExpanded, $"{label.text} ({count})", "BoldLabel");
+            if (!serializedProperty.isExpanded)
+            {
+                return;
+            }
+
+            if (reorderableList == null)
+            {
+                CreateReorderableList();
+            }
 
             rect.y += EditorGUIUtility.singleLineHeight;
-            rect.y += spaceHeight;
 
-            Rect button = rect;
-            button.x = rect.x + rect.width - buttonWidth;
-            button.height = EditorGUIUtility.singleLineHeight;
-            button.width = buttonWidth;
+            reorderableList.DoList(rect);
 
-            Rect folder = rect;
-            folder.width = folderWidth;
+            rect.y += GetHeightForAllPairs();
 
-            Rect rectNoFolder = rect;
-            rectNoFolder.width = rect.width - buttonWidth;
-
-            rect.width = rectNoFolder.width - folderWidth;
-            rect.x += folderWidth;
-
-            for (int i = 0; i < count; ++i)
-            {
-                SerializedProperty key = keys.GetArrayElementAtIndex(i);
-                SerializedProperty value = values.GetArrayElementAtIndex(i);
-
-                if (DrawAsSingleLine(key.propertyType, value.propertyType))
-                {
-                    button.y = rect.y;
-                    rectNoFolder.y = rect.y;
-                    Rect half = rectNoFolder;
-                    half.width = rectNoFolder.width * 0.5f;
-                    EditorGUI.PropertyField(half, key, GUIContent.none);
-                    half.x += half.width;
-                    EditorGUI.PropertyField(half, value, GUIContent.none);
-
-                    rect.y += EditorGUIUtility.singleLineHeight;
-                }
-                else
-                {
-                    float heightForKey = EditorGUI.GetPropertyHeight(key);
-
-                    rect.height = heightForKey;
-                    EditorGUI.PropertyField(rect, key, GUIContent.none);
-
-                    button.y = rect.y;
-                    folder.y = rect.y;
-
-                    rect.y += heightForKey;
-                    rect.height = heightForKey;
-
-                    string keyPropertyPath = key.propertyPath;
-                    if (!expandState.TryGetValue(keyPropertyPath, out bool expand))
-                    {
-                        expand = false;
-                    }
-
-                    if (expand)
-                    {
-                        float heightForValue = EditorGUI.GetPropertyHeight(value);
-                        rect.height = heightForValue;
-                        Color backgroundColor = GUI.backgroundColor;
-                        GUI.backgroundColor = lightGray;
-                        GUI.Box(rect, GUIContent.none);
-                        GUI.backgroundColor = backgroundColor;
-                        EditorGUI.PropertyField(rect, value, new GUIContent(value.type), value.hasVisibleChildren);
-
-                        rect.y += heightForValue;
-                    }
-
-                    expand = EditorGUI.Toggle(folder, string.Empty, expand, EditorStyles.foldout);
-                    expandState[keyPropertyPath] = expand;
-                }
-
-                if (GUI.Button(button, "x"))
-                {
-                    keys.DeleteArrayElementAtIndex(i);
-                    values.DeleteArrayElementAtIndex(i);
-                    return;
-                }
-
-                rect.y += spaceHeight;
-            }
-
-            rect.height = EditorGUIUtility.singleLineHeight;
             bool drawDropArea = typeof(UnityEngine.Object).IsAssignableFrom(valueType);
+
             if (property.FindPropertyRelative("invalidFlag").boolValue)
             {
-                rect.height = EditorGUIUtility.singleLineHeight;
-                EditorGUI.HelpBox(rect, "Duplicate keys exist", MessageType.Error);
+                //rect.height = EditorGUIUtility.singleLineHeight;
+                //EditorGUI.HelpBox(rect, "Duplicate keys exist", MessageType.Error);
             }
-            else
-            {
-                if (drawDropArea)
-                {
-                    rect.height = EditorGUIUtility.singleLineHeight * 2f;
-                    DrawDropArea(rect, property);
-                    rect.y += rect.height;
-                    rect.height = EditorGUIUtility.singleLineHeight;
-                }
 
-                DrawAddButton(rect, property);
+            if (drawDropArea)
+            {
+                rect.y += spaceHeight;
+                rect.height = dropAreaHeight;
+                DrawDropArea(rect, property);
+                rect.y += dropAreaHeight;
+            }
+
+            int selected = reorderableList.index;
+            if (selected >= 0 && selected < count)
+            {
+                SerializedProperty key = keys.GetArrayElementAtIndex(selected);
+                SerializedProperty value = values.GetArrayElementAtIndex(selected);
+                bool drawAsSingleLine = DrawAsSingleLine(key.propertyType, value.propertyType);
+                if (!drawAsSingleLine)
+                {
+                    rect.y += spaceHeight;
+                    rect.height = EditorGUI.GetPropertyHeight(value, GUIContent.none, value.isExpanded);
+                    EditorGUI.PropertyField(rect, value, GUIContent.none, value.isExpanded);
+                }
             }
         }
 
-        protected void DrawAddButton(Rect position, SerializedProperty property)
+        private float GetHeightForPair(SerializedProperty key, SerializedProperty value)
         {
-            Rect buttonRect = position;
-            buttonRect.width = Mathf.Min(position.width, 100);
-            buttonRect.x = position.center.x - buttonRect.width * 0.5f;
-            if (GUI.Button(buttonRect, "+"))
+            if (DrawAsSingleLine(key.propertyType, value.propertyType))
             {
-                SerializedProperty keys = property.FindPropertyRelative("keys");
-                SerializedProperty values = property.FindPropertyRelative("values");
-                int count = keys.arraySize;
-                keys.InsertArrayElementAtIndex(count);
-                values.InsertArrayElementAtIndex(count);
+                return EditorGUIUtility.singleLineHeight;
             }
+            else
+            {
+                return EditorGUI.GetPropertyHeight(key);
+            }
+        }
+
+        private float GetHeightForAllPairs()
+        {
+            float height = 0f;
+            SerializedProperty keys = serializedProperty.FindPropertyRelative("keys");
+            SerializedProperty values = serializedProperty.FindPropertyRelative("values");
+
+            height += reorderableList.headerHeight;
+
+            int count = keys.arraySize;
+
+            if (count != 0)
+            {
+                for (int i = 0; i < count; ++i)
+                {
+                    SerializedProperty key = keys.GetArrayElementAtIndex(i);
+                    SerializedProperty value = values.GetArrayElementAtIndex(i);
+                    height += GetHeightForPair(key, value);
+                }
+            }
+            else
+            {
+                height += emptyHeight;
+            }
+
+            height += reorderableList.footerHeight;
+            return height;
         }
 
         protected void DrawDropArea(Rect position, SerializedProperty property)
@@ -229,7 +294,7 @@ namespace AillieoUtils
             Rect dropArea = position;
             Color guiColor = GUI.color;
             GUI.color = Color.yellow;
-            GUI.Box(dropArea, "Drop objects hero to add new entries");
+            GUI.Box(dropArea, "Drop objects hero to add new entries", new GUIStyle("box") { alignment = TextAnchor.MiddleCenter });
             GUI.color = guiColor;
 
             switch (evt.type)
--- Tests/TestScript.cs
+++ Tests/TestScript.cs
@@ -2,7 +2,50 @@ using AillieoUtils;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class TestScript : MonoBehaviour
 {
+    [Serializable]
+    public class StringToString : SerializableDictionary<string, string>
+    {
+    }
+
+    [Serializable]
+    public class StringToObject : SerializableDictionary<string, UnityEngine.Object>
+    {
+    }
+
+    [Serializable]
+    public class Vector2ToAnimationCurve : SerializableDictionary<Vector2, AnimationCurve>
+    {
+    }
+
+    [Serializable]
+    public class Vector2ToObject : SerializableDictionary<Vector2, UnityEngine.Object>
+    {
+    }
+
+    [Serializable]
+    public class Vector4ToAnimationCurve : SerializableDictionary<Vector4, AnimationCurve>
+    {
+    }
+
+    [Serializable]
+    public class Vector4ToObject : SerializableDictionary<Vector4, UnityEngine.Object>
+    {
+    }
+
+    [Serializable]
+    public class Vector4ToVector4 : SerializableDictionary<Vector4, Vector4>
+    {
+    }
+
+    public StringToString stringToString;
+    public StringToObject stringToObject;
+    public Vector2ToAnimationCurve vector2ToAnimationCurve;
+    public Vector2ToObject vector2ToObject;
+    public Vector4ToAnimationCurve vector4ToAnimationCurve;
+    public Vector4ToObject vector4ToObject;
+    public Vector4ToVector4 vector4ToVector4;
 }
